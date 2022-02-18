6617010d92d554b49c5501040a0c5c0703cd017d
Arun <arunjoseph001@gmail.com>
2022-02-18 07:30:06 +0530

    Adding the game files

---
+++ script.js
@@ -0,0 +1,88 @@
+let playerSelection = "none"; //computerPlay();
+let computerSelection = "none";
+let winner = "none";
+let playerScore = 0;
+let computerScore = 0;
+
+const scores = document.querySelectorAll("strong");
+const clickMainImage = document.querySelector(".main img");
+const mainClass = document.querySelector(".main");
+const mainGame = document.querySelector(".game");
+const restart = document.querySelector("button");
+const clickImages = document.querySelector(".image");
+
+function computerPlay() {
+  // randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+  let computerChoice = Math.floor(Math.random() * 3);
+  if (computerChoice == 1) {
+    return "rock";
+  } else if (computerChoice == 2) {
+    return "paper";
+  } else return "scissors";
+}
+
+function playRound(player, computer) {
+  // returns who won the round
+  if (player == computer) {
+    return "tie";
+  } else if (player == "rock" && computer == "paper") {
+    computerScore++;
+    return "computer";
+  } else if (player == "paper" && computer == "scissors") {
+    computerScore++;
+    return "computer";
+  } else if (player == "scissors" && computer == "rock") {
+    computerScore++;
+    return "computer";
+  } else {
+    playerScore++;
+    return "user";
+  }
+}
+
+function game(event) {
+  // Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
+  // or  Just call your playRound function 5 times in a row. Loops are covered in the next lesson.
+  playerSelection = event.target.alt;
+
+  if (typeof playerSelection == "undefined") {
+    return;
+  }
+  computerSelection = computerPlay();
+  scores[0].innerText = computerSelection;
+  console.log("Computer choose " + computerSelection);
+  console.log("Player choose " + playerSelection);
+  winner = playRound(playerSelection, computerSelection);
+  console.log("winner is " + winner);
+  scores[1].innerText = playerScore;
+  scores[2].innerText = computerScore;
+  console.log("scores are " + playerScore + " and " + computerScore);
+  if(playerScore==5 || computerScore==5){
+    gameOver();
+  }
+}
+
+clickMainImage.addEventListener("click", () => {
+  console.log("cliked on the main image");
+  toggleDisplay();
+});
+
+restart.addEventListener("click", () => {
+  console.log("cliked on the restart button");
+  toggleDisplay();
+  playerScore = 0;
+  computerScore = 0;
+});
+
+clickImages.addEventListener("click", game);
+
+const toggleDisplay = () => {
+  mainClass.classList.toggle("display");
+  mainGame.classList.toggle("display");
+};
+
+const gameOver=()=>{
+  // need to stop game
+  clickImages.removeEventListener("click", game);
+
+}
